ce37c267b3d079789aa6e236bb75b2c8c7d770b0
Alex Alejandro <eaglesalejandro.aa@gmail.com>
2021-04-22 12:53:51 -0400

    created setDate and setDatePicker functions

--- script.js
+++ script.js
@@ -1,12 +1,24 @@
+import {format, getUnixTime, fromUnixTime } from 'date-fns'
+
+
 const datePickerBtn = document.querySelector('.date-picker-button')
 const datePicker = document.querySelector('.date-picker')
+const datePickerHeaderText = document.querySelector('.current-month')
 
 
 datePickerBtn.addEventListener('click', e => {
-    
     datePicker.classList.toggle('show')
+   const selectedDate = fromUnixTime(datePickerBtn.dataset.selectedDate)
+   setupDatePicker(selectedDate)
 })
 
 function setDate(date) {
-    datePickerBtn.innerText = 
+    datePickerBtn.innerText = format(date, 'MMMM do, yyyy')
+    datePickerBtn.dataset.selectedDate = getUnixTime(date)
 }
+
+function setupDatePicker(selectedDate){
+    console.log(selectedDate)
+}
+
+setDate(new Date())
